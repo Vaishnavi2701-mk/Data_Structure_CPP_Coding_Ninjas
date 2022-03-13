24f282eee881f4019efa5ca285d641214afc47c9
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-13 09:37:07 +0530

    Update and rename Recurssion2/printSubsetOfArray.cpp to Recursion-2/PrintSubsetOfArray.cpp

--- Recursion-2/PrintSubsetOfArray.cpp
+++ Recursion-2/PrintSubsetOfArray.cpp
@@ -1,5 +1,5 @@
 /* Print Subsets of Array
-Send Feedback
+
 Given an integer array (of length n), find and print all the subsets of input array.
 Subsets are of length varying from 0 to n, that contain elements of the array. But the order of elements should remain same as in the input array.
 Note : The order of subsets are not important. Just print the subsets in different lines.
