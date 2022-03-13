8907af2d6d7a5ce45344b9f7313b8a4ddd047543
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-13 10:42:24 +0530

    Update Delete-Node-Recursively.cpp

--- Linked List 1/Delete-Node-Recursively.cpp
+++ Linked List 1/Delete-Node-Recursively.cpp
@@ -1,42 +1,44 @@
 /*
 Delete Node Recursively
-Insert node (recursive)
-Send Feedback
-You have been given a linked list of integers. Your task is to write a function that inserts a node at a given position, 'pos'.
-Note:
+Given a singly linked list of integers and position 'i', delete the node present at the 'i-th' position in the linked list recursively.
+ Note :
 Assume that the Indexing for the linked list always starts from 0.
 
-If the given position 'pos' is greater than length of linked list, then you should return the same linked list without any change. And if position 'pos' is equal to length of input linked list, then insert the node at the last position.
-Input format:
+No need to print the list, it has already been taken care. Only return the new head to the list.
+ input format :
 The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
 
-The first line of each test case or query contains the elements of the linked list separated by a single space. 
+The first line of each test case or query contains the elements of the singly linked list separated by a single space.
 
-The second line of each test case contains two space separated integers, that denote the value of 'pos' and the data to be inserted respectively. 
- Remember/Consider :
+The second line of input contains a single integer depicting the value of 'i'.
+Remember/Consider :
 While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
-Output format:
-For each test case/query, print the resulting linked list of integers in a row, separated by a single space.
+Output format :
+For each test case/query, print the elements of the updated singly linked list.
 
-Output for every test case will be printed in a separate line.
-You don't need to print explicitly, it has been taken care of.
-Constraints:
-1 <= t <= 20
-0 <= length of linked list <= 10^4
-0 <= pos, data to be inserted <= 2^31 - 1
-Time Limit: 1 second
-Sample Input 1:
+Output for every test case will be printed in a seperate line.
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+0 <= i < M
+
+Time Limit:  1sec
+Sample Input 1 :
 1
 3 4 5 2 6 1 9 -1
-3 100
-Sample Output 1:
-3 4 5 100 2 6 1 9
-Sample Input 2:
-1 
-3 4 5 2 6 1 9 -1
-0 20
-Sample Output 2:
-20 3 4 5 2 6 1 9*/
+3
+Sample Output 1 :
+3 4 5 6 1 9
+Sample Input 2 :
+2
+30 -1
+0
+10 20 30 50 60 -1
+4
+Sample Output 2 :
+10 20 30 50 
+*/
 
 #include<iostream>
 using namespace std;
