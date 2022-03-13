0f3894e569b487705b153f3e8f904ec98e99c805
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-13 09:30:24 +0530

    Update Return-Subset-Of-an-Array.cpp

--- Recursion-2/Return-Subset-Of-an-Array.cpp
+++ Recursion-2/Return-Subset-Of-an-Array.cpp
@@ -1,5 +1,5 @@
 /* Return subset of an array
-Send Feedback
+
 Given an integer array (of length n), find and return all the subsets of input array.
 Subsets are of length varying from 0 to n, that contain elements of the array. But the order of elements should remain same as in the input array.
 Note : The order of subsets are not important.
