e9c75f652fccc22d72bec01c6471951b07ef7d72
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-13 09:36:18 +0530

    Update and rename Recurssion2/print_Subset_sum_to_k.cpp to Recursion-2/pPrint_Subset_sum_to_k.cpp

--- Recursion-2/pPrint_Subset_sum_to_k.cpp
+++ Recursion-2/pPrint_Subset_sum_to_k.cpp
@@ -1,5 +1,5 @@
 /* Print Subset Sum to K
-Send Feedback
+
 Given an array A and an integer K, print all subsets of A which sum to K.
 Subsets are of length varying from 0 to n, that contain elements of the array. But the order of elements should remain same as in the input array.
 Note : The order of subsets are not important. Just print them in different lines.
