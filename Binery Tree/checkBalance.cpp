b195635c43e45b8b1815725edb554a368ffe4eca
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-14 11:22:46 +0530

    Update checkBalance.cpp

--- Binery Tree/checkBalance.cpp
+++ Binery Tree/checkBalance.cpp
@@ -1,8 +1,32 @@
+/*
+Check Balanced
+
+Given a binary tree, check if it is balanced. Return true if given binary tree is balanced, false otherwise.
+Balanced Binary Tree:
+A empty binary tree or binary tree with zero nodes is always balanced. For a non empty binary tree to be balanced, following conditions must be met:
+1. The left and right subtrees must be balanced.
+2. |hL - hR| <= 1, where hL is the height or depth of left subtree and hR is the height or depth of right subtree.    
+Input format:
+The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have a left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.
+Output format
+The first and only line of output contains true or false.
+Constraints
+Time Limit: 1 second
+Sample Input 1 :
+5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+Sample Output 1 :
+false
+Sample Input 2 :
+1 2 3 -1 -1 -1 -1
+Sample Output 2 :
+true*/
+
 #include <iostream>
 #include<queue>
 using namespace std;
 #include "binarytreeRecurssiveClass.h"
 //1 2 3 4 5 -1 -1 -1 -1 6 7 -1 -1 -1 -1
+
 void printLevelWise(BinaryTreeNode<int> *root) {
     queue<BinaryTreeNode<int>*> printNodes;
 printNodes.push(root);
