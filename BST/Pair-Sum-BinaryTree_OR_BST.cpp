ea1cbba8c227df34e5a7cb9c47b3948329d00867
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-14 12:32:03 +0530

    Update and rename pairSumInBinaryTree_OR_BST.cpp to Pair-Sum-BinaryTree_OR_BST.cpp

--- BST/Pair-Sum-BinaryTree_OR_BST.cpp
+++ BST/Pair-Sum-BinaryTree_OR_BST.cpp
@@ -1,3 +1,25 @@
+/*
+Pair Sum Binary Tree
+
+Given a binary tree and an integer S, print all the pair of nodes whose sum equals S.
+Note:
+1. Assume the given binary tree contains all unique elements.
+2. In a pair, print the smaller element first. Order of different pairs doesn't matter.
+Input format :
+The first line of input contains data of the nodes of the tree in level order form. The data of the nodes of the tree is separated by space. If any node does not have a left or right child, take -1 in its place. Since -1 is used as an indication whether the left or right nodes exist, therefore, it will not be a part of the data of any node.
+The following line of input contains an integer, that denotes the value of S.
+Output Format :
+Print each pair in a new line, as described in the task. 
+Constraints:
+Time Limit: 1 second
+Sample Input 1:
+5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
+15
+Sample Output 1:
+5 10
+6 9
+*/
+
 #include <iostream>
 #include<vector>
 #include <queue>
