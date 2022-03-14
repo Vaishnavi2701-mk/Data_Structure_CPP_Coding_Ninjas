84207be73d5e98e9400e3276dbea5024844965b9
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-14 12:52:01 +0530

    Update and rename mergeKsortedArrays.cpp to MergeKsortedArrays.cpp

--- Priority Queues/MergeKsortedArrays.cpp
+++ Priority Queues/MergeKsortedArrays.cpp
@@ -1,3 +1,30 @@
+/*
+Merge K sorted arrays
+
+Given k different arrays, which are sorted individually (in ascending order). You need to merge all the given arrays such that output array should be sorted (in ascending order).
+Hint : Use Heaps.
+Input Format:
+The first line of input contains an integer, that denotes value of k.
+The following lines of input represent k sorted arrays. Each sorted array uses two lines of input. The first line contains an integer, that denotes the size of the array. The following line contains elements of the array.
+Output Format:
+The first and only line of output contains space separated elements of the merged and sorted array, as described in the task.
+Constraints:
+0 <= k <= 1000
+0 <= n1, n2 <= 10000
+Time Limit: 1 second
+Sample Input 1:
+4
+3
+1 5 9
+2
+45 90
+5
+2 6 78 100 234
+1
+0
+Sample Output 1:
+0 1 2 5 6 9 45 78 90 100 234
+*/
 #include<iostream>
 #include<queue>
 #include<vector>
