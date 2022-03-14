c0a522c2f1f5e5107bb9bffebf8a525e8b4aff4d
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-14 13:30:20 +0530

    Create Search-In-Tree.cpp

---
+++ Tries-and-Huffman/Search-In-Tree.cpp
@@ -0,0 +1,108 @@
+/*
+Code : Search in Tries
+
+Implement the function SearchWord for the Trie class.
+For a Trie, write the function for searching a word. Return true if the word is found successfully, otherwise return false.
+Note : main function is given for your reference which we are using internally to test the code.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    int choice;
+    cin >> choice;
+    Trie t;
+
+    while (choice != -1) {
+        string word;
+        bool ans;
+        switch (choice) {
+            case 1:  // insert
+                cin >> word;
+                t.insertWord(word);
+                break;
+            case 2:  // search
+                cin >> word;
+                cout << (t.search(word) ? "true\n" : "false\n");
+                break;
+            default:
+                return 0;
+        }
+        cin >> choice;
+    }
+
+    return 0;
+}
+
+lass TrieNode {
+   public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+
+    TrieNode(char data) {
+        this->data = data;
+        children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+    TrieNode *root;
+
+   public:
+    Trie() { 
+        root = new TrieNode('\0'); 
+    }
+
+    void insertWord(TrieNode *root, string word) {
+        // Base case
+        if (word.size() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+
+        // Small Calculation
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // Recursive call
+        insertWord(child, word.substr(1));
+    }
+
+    void insertWord(string word) { 
+        insertWord(root, word); 
+    }
+
+    bool search(TrieNode * root, string word) {
+         int index = word[0] - 'a';
+         //base case
+     if(root->children[index]==NULL)
+            return false;
+
+        if(word.size()==1)
+        {
+            return root->children[index]->isTerminal;
+        }
+
+        return search(root->children[index],word.substr(1)); 
+    }
+    
+    bool search(string word)
+    {  
+        return search(root,word);
+    }
+};
