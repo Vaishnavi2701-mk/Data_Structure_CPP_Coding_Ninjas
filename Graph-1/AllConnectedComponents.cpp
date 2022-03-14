19e1c1f161b8878e82a4bd5e9c03200c08864ea4
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-14 15:40:13 +0530

    Update and rename Graph/AllConnectedComponents.cpp to Graph-1/AllConnectedComponents.cpp

--- Graph-1/AllConnectedComponents.cpp
+++ Graph-1/AllConnectedComponents.cpp
@@ -1,3 +1,38 @@
+/*
+Code : All connected components
+
+Given an undirected graph G(V,E), find and print all the connected components of the given graph G.
+Note:
+1. V is the number of vertices present in graph G and vertices are numbered from 0 to V-1. 
+2. E is the number of edges present in graph G.
+3. You need to take input in main and create a function which should return all the connected components. And then print them in the main, not inside function.
+Print different components in new line. And each component should be printed in increasing order (separated by space). Order of different components doesn't matter.
+Input Format :
+The first line of input contains two integers, that denote the value of V and E.
+Each of the following E lines contains two space separated integers, that denote that there exists an edge between vertex a and b.
+Output Format :
+Print different components in new line. And each component should be printed in increasing order of vertices (separated by space). Order of different components doesn't matter.
+Constraints :
+0 <= V <= 1000
+0 <= E <= (V * (V - 1)) / 2
+0 <= a <= V - 1
+0 <= b <= V - 1
+Sample Input 1:
+4 2
+0 1
+2 3
+Sample Output 1:
+0 1 
+2 3 
+Sample Input 2:
+4 3
+0 1
+1 3 
+0 3
+Sample Output 2:
+0 1 3 
+2*/
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
