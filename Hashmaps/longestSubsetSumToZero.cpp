f4162c00f34efdbfc3cadf404806299e17c47f45
Vaishnavi Mokadam <89184872+Vaishnavi2701-mk@users.noreply.github.com>
2022-03-14 13:03:02 +0530

    Update and rename unorderd_map_hashtable/longestSubsetSumToZero.cpp to Hashmaps/longestSubsetSumToZero.cpp

--- Hashmaps/longestSubsetSumToZero.cpp
+++ Hashmaps/longestSubsetSumToZero.cpp
@@ -1,3 +1,24 @@
+/*
+Longest subset zero sum
+
+Given an array consisting of positive and negative integers, find the length of the longest subarray whose sum is zero.
+Input Format:
+The first line of input contains an integer, that denotes the value of the size of the array. Let us denote it with the symbol N.
+The following line contains N space separated integers, that denote the value of the elements of the array.
+Output Format
+The first and only line of output contains length of the longest subarray whose sum is zero.
+Constraints:
+0 <= N <= 10^8
+Time Limit: 1 sec
+Sample Input 1:
+10 
+ 95 -97 -387 -435 -5 -70 897 127 23 284
+Sample Output 1:
+5
+Explanation:
+The five elements that form the longest subarray that sum up to zero are: -387, -435, -5, -70, 897 
+*/
+
 #include <iostream>
 using namespace std;
 #include<unordered_map>
@@ -56,11 +77,7 @@ int main() {
 
 
 
-Abhjit Chavda
 
-Theoretical physicist, historian, technologist and writer
-
-he authered several research papers
 
 
 
